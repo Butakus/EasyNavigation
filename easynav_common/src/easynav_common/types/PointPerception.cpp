176bbbeb755c4f11dda3d13fb3d4822b036ae5c4
Francisco Martín Rico <fmrico@gmail.com>
2026-02-18 09:49:04 +0100

    GPLv3 -> Apache 2.0
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- easynav_common/src/easynav_common/types/PointPerception.cpp
+++ easynav_common/src/easynav_common/types/PointPerception.cpp
@@ -1,21 +1,16 @@
 // Copyright 2025 Intelligent Robotics Lab
 //
-// This file is part of the project Easy Navigation (EasyNav in short)
-// licensed under the GNU General Public License v3.0.
-// See <http://www.gnu.org/licenses/> for details.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Easy Navigation program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <string>
 #include <vector>
@@ -644,7 +639,15 @@ PointPerceptionsOpsView::fuse(
         exact_time ? tf2_ros::fromMsg(pptr->stamp) : tf2::TimePointZero,
         tf2::durationFromSec(0.0));
 
-      stamp = tf_msg.header.stamp;
+      if (exact_time) {
+        if (stamp < rclcpp::Time(pptr->stamp, stamp.get_clock_type())) {
+          stamp = rclcpp::Time(pptr->stamp, stamp.get_clock_type());
+        }
+      } else {
+        if (stamp < rclcpp::Time(tf_msg.header.stamp, stamp.get_clock_type())) {
+          stamp = rclcpp::Time(tf_msg.header.stamp, stamp.get_clock_type());
+        }
+      }
 
       tf2::fromMsg(tf_msg.transform, tf_transforms_[i]);
       tf_valid_[i] = true;
